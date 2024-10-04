057b8d975ed63f7a74f25b1b000a031d947ef0ad
Ethanhuang-aml <sijunhuang126@gmail.com>
2024-10-04 03:03:53 -0700

    finish week3 assignment

--- week3-assignment/script.js
+++ week3-assignment/script.js
@@ -1,35 +1,42 @@
-let DOMsection=document.getElementById("DOMoptions")
-let newParagraph=document.createElement("p")
-let buttonColor=document.getElementById("btnColorChange")
-let toggleImage=document.getElementById("btnImageToggle")
-let galleryIMG=document.getElementById("imageGallery").children[0]
+let toggleImage1=document.getElementById("btn1")
+let toggleImage2=document.getElementById("btn2")
+let toggleImage3=document.getElementById("btn3")
+let toggleImage4=document.getElementById("btn4")
+ 
+let imageGallery = document.querySelector("#imageGallery img");
 
+toggleImage1.addEventListener("click", function() {
+    let duck1 = "images/duck1.jpg";  
+    console.log("fire!");               
+    console.log(duck1);              
 
+    imageGallery.src = duck1;        
+    console.log("Image switched to duck1");
+});
 
-newParagraph.innerText="Hello World"
-DOMsection.appendChild(newParagraph)
+toggleImage2.addEventListener("click", function() {
+    let duck2 = "images/duck2.jpg";  
+    console.log("fire!");               
+    console.log(duck2);              
 
-let imageToggle=function(){
-    console.log("fire!")
-    console.log(galleryIMG.src)
-    if(galleryIMG.src.includes("gallery1")){
-        console.log("gallery1")
-        galleryIMG.src="images/gallery2.jpg"
-    }
-    else{
-        console.log("gallery2")
-        galleryIMG.src="images/gallery1.jpg"
-    }
-}
+    imageGallery.src = duck2;        
+    console.log("Image switched to duck2");
+});
 
-toggleImage.addEventListener("click",imageToggle)
-buttonColor.addEventListener("click",function(){
-    let redPortion =Math.random()*255
-    let greenPortion=Math.random()*255
-    let bluePortion=Math.random()*255
+toggleImage3.addEventListener("click", function() {
+    let duck3 = "images/duck3.jpg";  
+    console.log("fire!");               
+    console.log(duck3);              
 
-    let randomColor="rgb("+ redPortion +", "+ greenPortion +", "+ bluePortion +")"
-    console.log(randomColor)
+    imageGallery.src = duck3;        
+    console.log("Image switched to duck3");
+});
 
-    DOMsection.style.backgroundColor=randomColor
-})
+toggleImage4.addEventListener("click", function() {
+    let duck4 = "images/duck4.jpg";  
+    console.log("fire!");               
+    console.log(duck4);              
+
+    imageGallery.src = duck4;        
+    console.log("Image switched to duck4");
+});
